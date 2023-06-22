00f39687b560fd1b089156c7f2810b2c7884ac59
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-06-22 15:28:38 -0300

    Update Atvd09.java

--- Atividades/Atv09/Notebook/Atvd09.java
+++ Atividades/Atv09/Notebook/Atvd09.java
@@ -1,28 +1,34 @@
-//inicio do método imprimirNumeros
-public int imprimirNumeros(int[] imprimir_array = new int[10]){ //inicializando os parametros do método
-    return imprimir_array; //retorna o array 
-} //fim do método imprimirNumeros
+public class exercicioSemRetorno{ //inicio da classe ExercicioSemRetorno
+
+    //inicio do método imprimirNumeros
+    public void imprimirNumeros(int[] imprimir_array){ //inicializando os parametros do método
+        String aux = ""; 
+        for (int i = 0; i <= 9; i++) { //incio do for que percorre os itens de 0 a 10
+             aux += (imprimir_array[i] + " "); 
+        }     
+        System.out.println(aux); //retorna o array 
+    } //fim do método imprimirNumeros
 
 
-//inicio do método somaNumeros
-public int somaNumeros(int[] soma_array = new int[10]){ //inicializando os parametros do método
+    //inicio do método somaNumeros
+    public void somaNumeros(int[] soma_array){ //inicializando os parametros do método
 
-    int aux = 0; //contador para o for
+        int aux = 0; //contador para o for
 
-    for (int i = 0; i <= 10; i++) { //incio do for que percorre os itens de 0 a 10
-        int aux += soma_array[i]; //variavel aux vai receber a soma dos elementos da lista
-    }//fim do for
-    return soma; //retorna a variavel soma 
-}//fim do método somaNumeros
+        for (int i = 0; i <= 9; i++) { //incio do for que percorre os itens de 0 a 10
+             aux += soma_array[i]; //variavel aux vai receber a soma dos elementos da lista
+        }//fim do for
+        System.out.println(aux); //retorna a variavel soma 
+    }//fim do método somaNumeros         
 
-//inicio da classe ExercicioSemRetorno
-public class ExercicioSemRetorno{  
-    public static void main  (String [] args) { //incio do método main
-        int numeros = [10, 20, 30, 46, 70, 32, 45, 65, 1, 2]; //array com os numeros inteiros definidos
+    public static void main(String [] args) { //incio do método main
+        exercicioSemRetorno x = new exercicioSemRetorno(); 
 
-        int  primeira = imprimirNumeros(numeros);   //chama o método imprimirNumeros e utiliza os numeros do array como parametro
-        System.out.println("Os elementos do vetor são: " + primeira); //mostra os números contidos no array
-        int  segunda = somaNumeros(numeros); //chama o método somaNumeros e utiliza o array numeros como parametro
-        System.out.println("A soma dos números desse vetor é: " + segunda); //mostra a soma dos numeros do array numeros
+        int[] numerosA = {10, 20, 30, 46, 70, 32, 45, 65, 1, 2}; //array com os numeros inteiros definidos
+        //int[] numerosB = {10, 20, 30, 46, 70, 32, 45, 65, 1, 2}; //array com os numeros inteiros definidos
+
+        
+        x.imprimirNumeros(numerosA);
+        x.somaNumeros(numerosA);
     }//fim do método main
 } //fim da classe ExercicioSemRetorno
