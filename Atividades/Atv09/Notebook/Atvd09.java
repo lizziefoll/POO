45f244f784b2fa727bcc5061e708b6581a88cbc9
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-06-23 19:58:00 -0300

    Update Atvd09.java

--- Atividades/Atv09/Notebook/Atvd09.java
+++ Atividades/Atv09/Notebook/Atvd09.java
@@ -1,4 +1,13 @@
-public class exercicioSemRetorno{ //inicio da classe ExercicioSemRetorno
+/*
+Classe impressão de texto: "ExercicioSemRetorno.java"
+IFPR - Campus Cascavel
+Disciplina: Programação orientada a Objetos
+Professor: Nelson Bellincanta
+Aluna: Elisabeth Folador Ferreira
+*/
+
+import java.util.ArrayList;
+public class ExercicioSemRetorno{ //inicio da classe ExercicioSemRetorno
 
     //inicio do método imprimirNumeros
     public void imprimirNumeros(int[] imprimir_array1, int[] imprimir_array2){ //inicializando os parametros do método
@@ -12,30 +21,43 @@ public class exercicioSemRetorno{ //inicio da classe ExercicioSemRetorno
         System.out.println("Segunda lista: "+aux2); 
     } //fim do método imprimirNumeros
 
+     
+
 
     //inicio do método somaNumeros
     public void somaNumeros(int[] soma_array1, int[] soma_array2){ //inicializando os parametros do método
 
-        int soma = 0; //contador para o for
-        int[] soma_array3 = new int[10]; 
-        int cont = 0; 
+        int soma = 0; //variavel que vai receber a soma da lista
+        int[] soma_array3 = new int[10]; //cria uma lista de 10 elementos
+        int cont; //cria a variavel
 
-        for (int i = 0; i <= 9; i++){ //incio do for que percorre os itens de 0 a 10
-            cont = soma_array1[i] + soma_array2[i]; 
-            soma += cont; //variavel aux vai receber a soma dos elementos da lista
-            soma_array3[i] = soma; 
+        for (int i = 0; i <= 9; i++){ //incio do for que percorre os itens da posição 0 a 9
+            cont = soma_array1[i] + soma_array2[i]; //contador recebe a soma das duas listas 
+            soma += cont; //a variavel soma ira receber a soma dos elementos das duas listas
+            soma_array3[i] = cont; //adicionando ao soma_array3 a soma das listas 1 e 2
         }//fim do for
-        System.out.println(soma); //retorna a variavel soma 
+        System.out.println("A soma dos números dessa lista é: "+ soma); //retorna a variavel soma 
+
+
+        String aux = ""; //cria uma variavel auxiliar
+        for (int i = 0; i <= 9; i++) { //incio do for que percorre os itens de 0 a 10
+            aux += (soma_array3[i] + " "); //imprime os numeros da lista separados por um espaço sem virgula
+        }//fim do for
+
+        System.out.println("A soma das duas listas é igual a: "+ aux); //mostra a soma das duas listas
+
+
     }//fim do método somaNumeros         
 
+
     public static void main(String [] args) { //incio do método main
-        exercicioSemRetorno x = new exercicioSemRetorno(); 
+        ExercicioSemRetorno x = new ExercicioSemRetorno(); 
 
         int[] numerosA = {10, 20, 30, 46, 70, 32, 45, 65, 1, 2}; //array com os numeros inteiros definidos
         int[] numerosB = {1, 2, 3, 4, 7, 3, 4, 6, 1, 2}; //array com os numeros inteiros definidos
 
         
-        x.imprimirNumeros(numerosA, numerosB);
-        x.somaNumeros(numerosA, numerosB);
+        x.imprimirNumeros(numerosA, numerosB);//imprime as duas listas utilizadas nas operações
+        x.somaNumeros(numerosA, numerosB); //imprime a soma das duas listas utilizadas nas operações 
     }//fim do método main
 } //fim da classe ExercicioSemRetorno
