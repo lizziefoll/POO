0c4dda98487e85b00215aeb514bad7f2eac00597
Elisabeth <124798717+lizziefoll@users.noreply.github.com>
2023-07-27 15:02:23 -0300

    Create C3.java

---
+++ Package/C3.java
@@ -0,0 +1,9 @@
+package pacote1; 
+public class C3 {
+    public static void main(String[] args){
+        C1 x = new C1();
+        C2 y = new C2();
+        System.out.println(x.somar(a:6, b:2)); 
+        System.out.println(y.subtrair(a:6, 2)); 
+    }
+}
